960666fe5b5e27da1bffb82d708aeec009ef8e5c
siddhusalvi <siddheshsalvi525@gmail.com>
2020-07-15 11:16:35 +0530

    ADDED : TestCase 1.1

--- src/test/scala/analyser/MoodAnalyserTest.scala
+++ src/test/scala/analyser/MoodAnalyserTest.scala
@@ -7,9 +7,9 @@ import org.junit.Assert._
 class MoodAnalyserTest extends TestCase{
 
   @Test
-  def test1(): Unit ={
+  def testCase1 1(): Unit ={
    val m:MoodAnalyser = new MoodAnalyser()
-    val msg = "Mood changes everything"
+    val msg = "I am in Sad\nMood"
     assertEquals(m.analyseMood(msg),"SAD")
 
   }
