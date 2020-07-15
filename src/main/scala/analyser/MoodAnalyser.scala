bf2815d0091adf1584b191931db38708bb588706
siddhusalvi <siddheshsalvi525@gmail.com>
2020-07-15 11:51:29 +0530

    ADDED :

--- src/main/scala/analyser/MoodAnalyser.scala
+++ src/main/scala/analyser/MoodAnalyser.scala
@@ -1,7 +1,13 @@
 package analyser
 
 class MoodAnalyser{
-  def analyseMood(msg:String):String = {
-    if (msg.toLowerCase.contains("sad")) {"SAD"}else{ "HAPPY"}
+  var msg = ""
+  def this(msg:String){
+    this()
+    this.msg = msg
+  }
+
+  def analyseMood():String = {
+    if (this.msg.toLowerCase.contains("sad")) {"SAD"}else{ "HAPPY"}
  }
 }
